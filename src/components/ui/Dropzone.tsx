c2361a10b21108ac26b3e3d3e1c5fdd686fefdd2
kazisean <keh8423@nyu.edu>
2025-10-20 09:47:00 -0400

    fixed inconsistent formating

--- src/components/ui/Dropzone.tsx
+++ src/components/ui/Dropzone.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useDropzone, FileRejection } from 'react-dropzone';
+import React from "react";
+import { useDropzone, FileRejection } from "react-dropzone";
 
 interface DropzoneProps {
   onDrop: (acceptedFiles: File[], rejectedFiles: FileRejection[]) => void;
@@ -9,7 +9,7 @@ const Dropzone: React.FC<DropzoneProps> = ({ onDrop }) => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     accept: {
-      'image/*': ['.jpeg', '.jpg', '.png'],
+      "image/*": [".jpeg", ".jpg", ".png"],
     },
   });
 
@@ -17,12 +17,14 @@ const Dropzone: React.FC<DropzoneProps> = ({ onDrop }) => {
     <div
       {...getRootProps()}
       className={`w-full sm:w-[400px] md:w-[600px] lg:w-[800px] h-[200px] sm:h-[300px] lg:h-[400px] border-2 ${
-        isDragActive ? 'border-blue-400' : 'border-gray-300'
+        isDragActive ? "border-blue-400" : "border-gray-300"
       } border-dashed rounded-lg flex items-center justify-center bg-transparent backdrop-blur-lg hover:bg-gray-100 cursor-pointer`}
     >
       <input {...getInputProps()} />
       {isDragActive ? (
-        <p className="text-gray-600 text-sm sm:text-base">Drop the image here...</p>
+        <p className="text-gray-600 text-sm sm:text-base">
+          Drop the image here...
+        </p>
       ) : (
         <p className="text-gray-600 text-sm sm:text-base">
           Drag & drop an image here, or click to select one
